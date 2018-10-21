ab5c458adf08754b068e688ed45ccb667db8ba00
BigDataTeacher <564515402@qq.com>
2018-10-21 15:16:47 +0800

    公共异常类

--- src/main/java/com/tecode/exception/BaseException.java
+++ src/main/java/com/tecode/exception/BaseException.java
@@ -1,7 +1,26 @@
 package com.tecode.exception;
 
 /**
- * Created by Administrator on 2018/10/21.
+ * 自定义异常类
  */
-public class BaseException {
+public class BaseException extends  Exception {
+    public BaseException() {
+        super();
+    }
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
+
+    protected BaseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
