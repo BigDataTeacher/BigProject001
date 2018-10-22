9a3612de3fa92ddfaad22581e6547fb4ce943459
BigDataTeacher <564515402@qq.com>
2018-10-22 11:04:13 +0800

    G01First

--- src/main/java/com/tecode/g01/controller/UserController.java
+++ src/main/java/com/tecode/g01/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.tecode.g01.controller;
 
 import com.tecode.bean.User;
-import com.tecode.service.UserService;
+import com.tecode.g01.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
