80112b3e5526dc6f15d1995a61960b51acfa9b0a
BigDataTeacher <564515402@qq.com>
2018-10-22 18:22:52 +0800

    修改：增加对输入空格的判断

--- src/main/java/com/tecode/g02/controller/G02ZXLNameController.java
+++ src/main/java/com/tecode/g02/controller/G02ZXLNameController.java
@@ -60,9 +60,8 @@ public class G02ZXLNameController {
         //创建一个map集合用来存放返回的结果
         Map<String,Object> map=new HashedMap();
 
-
         //对传入的姓名进行判断，如果为空则直接返回，不为空则调用Service层中查找用户的方法
-        if(name!=null&&!name.equals("")){
+        if(name!=null&&!name.matches("[ ]+")&&!name.equals("")){
             try {
                 //获取从Service层返回的结果
                 List<User> userList=userService.findUser(name);
