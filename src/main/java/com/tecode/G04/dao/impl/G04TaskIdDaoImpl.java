66b6813935bb32e79c48f6c26458eb798667b62a
BigDataTeacher <564515402@qq.com>
2018-10-22 14:35:09 +0800

    集群配置

--- src/main/java/com/tecode/G04/dao/impl/G04TaskIdDaoImpl.java
+++ src/main/java/com/tecode/G04/dao/impl/G04TaskIdDaoImpl.java
@@ -1,7 +1,16 @@
 package com.tecode.G04.dao.impl;
 
+import com.tecode.G04.dao.G04TaskIdDao;
+import com.tecode.bean.Task;
 import com.tecode.bean.User;
 import com.tecode.dao.UserDao;
+import com.tecode.util.hbase.table.HBaseUtils;
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,11 +19,31 @@ import org.springframework.stereotype.Repository;
  *   需要类上添加@Repository注解
  */
 @Repository
-public class G04TaskIdDaoImpl implements UserDao {
+public class G04TaskIdDaoImpl implements G04TaskIdDao {
 
 
     @Override
-    public User getUserByUserName(String username) throws Exception {
+    public Task modifyFinishState(String taskId,String cusId) throws Exception {
+
+        //获得Hbase链接
+        Connection conn = HBaseUtils.getConnection();
+        //得到表
+        Table table = conn.getTable(TableName.valueOf("task"));
+        //g构建扫描器
+       Scan scan = new Scan();
+
+
+
+        Admin admin = conn.getAdmin();
+
+        HTableDescriptor descriptor = admin.getTableDescriptor(TableName.valueOf("task"));
+
+
+
+
+        Task task= new Task();
+        //获得任务状态
+        task.getTaskState();
 
 
 
--- src/main/java/com/tecode/G04/service/G04TaskService.java
+++ src/main/java/com/tecode/G04/service/G04TaskService.java
@@ -4,23 +4,24 @@ package com.tecode.G04.service;
  * Created by Administrator on 2018/10/18.
  */
 
+import com.tecode.bean.Task;
 import com.tecode.bean.User;
 
 /**
  *
  * 处理用户信息相关的业务层
  */
-public interface G04UserService {
+public interface G04TaskService {
 
     /**
      * 用户登录的业务处理方法
-     * @param user
+     * @param taskId
      * @return
      */
-    User getUseLogin(User user);
+    Task getUseLogin(Task  taskId);
 
     /**
      *根据用户名id查询用户对象
      */
-    User getUserByUserName(String username) throws Exception;
+    Task getUserByUserName(String taskId) throws Exception;
 }
