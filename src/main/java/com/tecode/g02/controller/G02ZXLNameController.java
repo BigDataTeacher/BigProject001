a0340a5c6d73c250927095422466aaa5e6f8d4da
BigDataTeacher <564515402@qq.com>
2018-10-23 10:13:04 +0800

    人员查询

--- src/main/java/com/tecode/g02/controller/G02ZXLNameController.java
+++ src/main/java/com/tecode/g02/controller/G02ZXLNameController.java
@@ -23,7 +23,7 @@ import java.util.Map;
 @Controller
 public class G02ZXLNameController {
     /**
-     *需要调用业务层（Services)的方法时 声明的对象 类型为接口， 添加@Autowired，实现对该对象的实例化。
+     * 需要调用业务层（Services)的方法时 声明的对象 类型为接口， 添加@Autowired，实现对该对象的实例化。
      */
     @Autowired
     private G02ZXLUserService userService;
@@ -31,18 +31,18 @@ public class G02ZXLNameController {
 
     /**
      * 用户登录方法
-     *
+     * <p>
      * 1.其中 @RequestMapping(value = "/userLogin", method = RequestMethod.POST) 表示html页面请求到该方法的URL地址的映射
-     *      value = "/userLogin" ： html的请求地址
-     *      method = RequestMethod.POST：表示请求的方式
+     * value = "/userLogin" ： html的请求地址
+     * method = RequestMethod.POST：表示请求的方式
      * 2.@RequestBody:表示接收josn类型的参数
-     *          注意User对象中的属性名称，必须和html页面传递的参数的名称完全相同，包括大小写。
-     *
+     * 注意User对象中的属性名称，必须和html页面传递的参数的名称完全相同，包括大小写。
+     * <p>
      * 3.@ResponseBody:：表示把返回的值封装成json进行返回
      */
     @ResponseBody
     @RequestMapping(value = "/selectperson", method = RequestMethod.POST)
-    public Map<String,Object> getUserByName(User user){
+    public Map<String, Object> getUserByName(User user) {
         /**
          *1.验证参数的合法性
          * 2.调用业务逻辑层处理业务，并获得返回值
@@ -55,24 +55,24 @@ public class G02ZXLNameController {
          *
          */
         //用一个变量来接收传入的姓名
-        String name=user.getName();
+        String name = user.getName();
 
         //创建一个map集合用来存放返回的结果
-        Map<String,Object> map=new HashedMap();
+        Map<String, Object> map = new HashedMap();
 
         //对传入的姓名进行判断，如果为空则直接返回，不为空则调用Service层中查找用户的方法
-        if(name!=null&&!name.matches("[ ]+")&&!name.equals("")){
+        if (name != null && !name.matches("[ ]+") && !name.equals("")) {
             try {
-                //获取从Service层返回的结果
-                List<User> userList=userService.findUser(name);
+                //调取Service的查找用户的方法并获得返回结果
+                List<User> userList = userService.findUser(name);
 
-                //如果结果为空，则返回false，不为空返回true和list集合
-                if(userList.size()==0||userList==null){
-                    map.put("success",false);
-                    map.put("msg","查找不到此用户");
-                }else{
-                    map.put("success",true);
-                    map.put("data",userList);
+                //如果结果为空，则返回false和错误消息，不为空返回true和list集合
+                if (userList.size() == 0 || userList == null) {
+                    map.put("success", false);
+                    map.put("msg", "查找不到此用户");
+                } else {
+                    map.put("success", true);
+                    map.put("data", userList);
                 }
                 return map;
             } catch (IOException e) {
@@ -81,8 +81,8 @@ public class G02ZXLNameController {
 
         }
         //如果输入的参数不符合规则，返回false并加上错误提示
-        map.put("success",false);
-        map.put("msg","输入的名字不能为空");
+        map.put("success", false);
+        map.put("msg", "输入的名字不能为空");
 
         return map;
     }
